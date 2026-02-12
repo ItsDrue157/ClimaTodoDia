f223f190255c899929a18949916a177160cdc1cf
Carlos <carlosmateus7720@gmail.com>
2026-02-11 22:46:24 -0300

    1.0

--- 102.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-meuDados = {'name': 'Carlos', 'curso': 'Python Request'}
-
-response = requests.post('https://httpbin.org/post', json=meuDados)
-
-print(response.json()['json'])
--- app.py
+++ app.py
@@ -1,19 +1,63 @@
-import requests 
+import requests
+from datetime import datetime
+import time
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
-weather_url = "https://api.weatherapi.com/v1/current.json?key=2b8057a8e0674a46bf4144319260902&q=Rio de Janeiro&aqi=no"
-
-try:
-    response = requests.get(weather_url)
-    if response.status_code == 200:
-        print("Sucesso")
-        conteudo = response.json()
-        print('Temperatura abaixo')
-        print(conteudo['current']['temp_c'])
-    else:
-        print("Sem Acesso")
-except:
-    print("Erro de conexão, verifique a internet")
+app = Flask(__name__)
 
 
 
+def dadosDeLocalizacao(dados_recebidos):
+    resultado = dados_recebidos['results'][0]
+    lat = resultado['latitude']
+    long = resultado['longitude']
+    
+    return lat,long
+
+
+def clima(lat,long):
+    urlLoc = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto&forecast_days=1'
+    response = requests.get(urlLoc)
+    dadosClima = response.json()
+
+    maxima = dadosClima['daily']['temperature_2m_max'] [0]
+    minima = dadosClima['daily']['temperature_2m_min'][0]
+    chuva = dadosClima['daily']['precipitation_probability_max'][0]
+
+    return maxima, minima, chuva
+
+
+
+
+
+@app.route("/bot", methods=['POST'])
+def bot():
+    
+    
+    # 1. Pegar a mensagem que o usuário enviou
+    msg_usuario = request.values.get('Body', '').lower()
+
+    
+    
+    # 2. Criar o objeto de resposta do Twilio
+    urlGeo = f'https://geocoding-api.open-meteo.com/v1/search?name={msg_usuario}&count=1&language=pt&format=json'
+    responseGeo = requests.get(urlGeo)
+    conteudoGeo = responseGeo.json()
+
+    lat, long = dadosDeLocalizacao(conteudoGeo)
+    
+    temperaturaMaxima, temperaturaMinima, pobraChuva = clima(lat, long)
+    mensagem = (f'previsão para {msg_usuario}\n'
+                f'Maxima de {temperaturaMaxima}\n'
+                f'Minima de {temperaturaMinima}\n'
+                f'e a Chance de chuva é de {pobraChuva}')
+    
+    
+    resp = MessagingResponse()
+    resp.message(mensagem)
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(port=5000)
 
